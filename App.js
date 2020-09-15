c0060b6bf0861ca75e022a937218fbbf9b4c7584
Santosh Varma Chintalapati <svchintalapati@sst.local>
2020-09-15 10:18:35 +0530

    =fix

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT||3000
 app.listen(port,( )=>{
    console.log('App server start listening to port ' + port)
 })
 
 app.get('',(req,resp)=>{
-    resp.render('<h1>This is help page!!!</h1>');
+    resp.send('<h1>This is help page!!!</h1>');
 })
