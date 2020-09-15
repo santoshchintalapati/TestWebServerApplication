b8fb7b2b93b9a38577615ea1acecf58106a96d14
Santosh Varma Chintalapati <svchintalapati@sst.local>
2020-09-15 10:04:49 +0530

    =fixing port problem

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 app.listen(port,(req,resp)=>{
-   console.log('App server start listening to port ' + PORT)
+   console.log('App server start listening to port ' + port)
 })
